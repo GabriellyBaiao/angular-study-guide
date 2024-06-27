3b7381476c0487bbf03cb65c287509ebec0ce66d
Gabrielly Baião <103866012+GabriellyBaiao@users.noreply.github.com>
2024-06-27 18:30:37 -0300

    [PTI] feat: Using preprocessors (Sass, Less, Stylus)

--- 04_diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
+++ 04_diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-diretiva-ngif',
   templateUrl: './diretiva-ngif.component.html',
-  styleUrl: './diretiva-ngif.component.css'
+  styleUrl: './diretiva-ngif.component.scss'
 })
 export class DiretivaNgifComponent implements OnInit {
 
