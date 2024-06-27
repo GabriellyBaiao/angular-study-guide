91c25b79fc4e4574b08967cc21c54bea79926f43
Gabrielly Baião <103866012+GabriellyBaiao@users.noreply.github.com>
2024-06-27 17:13:55 -0300

    [PTI] feat: DOM and Template Access with ViewChild

--- data-biding/src/app/output-property/output-property.component.ts
+++ data-biding/src/app/output-property/output-property.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'contador',
@@ -12,13 +12,16 @@ export class OutputPropertyComponent {
 
   @Output() mudouValor = new EventEmitter();
 
+  @ViewChild('campoInput')
+  campoValorInput!: ElementRef;
+
   incrementa(){
-    this.valor++;
+    this.campoValorInput.nativeElement.value++;
     this.mudouValor.emit({novoValor: this.valor})
   }
 
   decrementa(){
-    this.valor--;
+    this.campoValorInput.nativeElement.value--;
     this.mudouValor.emit({novoValor: this.valor})
   }
 
