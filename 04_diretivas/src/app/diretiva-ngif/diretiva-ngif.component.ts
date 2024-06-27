3b5fab0d8a2ab67a9bd43de213548df842ff9b0e
Gabrielly Baião <103866012+GabriellyBaiao@users.noreply.github.com>
2024-06-27 18:04:15 -0300

    [PTI] feat: Creating components, services: ng generate

---
+++ 04_diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-diretiva-ngif',
+  templateUrl: './diretiva-ngif.component.html',
+  styleUrl: './diretiva-ngif.component.css'
+})
+export class DiretivaNgifComponent implements OnInit {
+
+  constructor(){}
+
+    ngOnInit(){
+    }
+    
+}
