2d714c9a94e808e9b6c23d4582a97d91525f5601
Gabrielly Baião <103866012+GabriellyBaiao@users.noreply.github.com>
2024-06-30 15:46:43 -0300

    [PTI] feat:  Directives: ngSwitch, ngSwitchCase e ngSwitchDefault

---
+++ 04_diretivas/src/app/diretiva-ngswitch/diretiva-ngswitch.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-diretiva-ngswitch',
+  templateUrl: './diretiva-ngswitch.component.html',
+  styleUrl: './diretiva-ngswitch.component.css'
+})
+export class DiretivaNgswitchComponent {
+
+  aba: string = 'home';
+
+  constructor(){}
+
+  ngOnInit(){
+
+  }
+
+}
