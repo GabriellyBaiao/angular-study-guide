e1d694f88bfb1d9dc28b9af936df3a7d4729e428
Gabrielly Baião <103866012+GabriellyBaiao@users.noreply.github.com>
2024-07-01 20:35:40 -0300

    [PTI] feat: Injecting a service into another service

--- 05_servico/src/app/app.module.ts
+++ 05_servico/src/app/app.module.ts
@@ -5,7 +5,8 @@ import { AppComponent } from './app.component';
 import { CursosModule } from './cursos/cursos.module';
 import { CursosService } from './cursos/cursos.service';
 import { CriarCursoModule } from './criar-curso/criar-curso.module';
-// import { LogService } from './shared/log.service';
+import { LogService } from './shared/log.service';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +17,7 @@ import { CriarCursoModule } from './criar-curso/criar-curso.module';
     CriarCursoModule,
     CursosModule
   ],
-  // providers: [LogService],
+  providers: [LogService],
   //providers: [CursosService],
   bootstrap: [AppComponent]
 })
