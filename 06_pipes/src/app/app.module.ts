517069271ee35dec29db5272fdfe8c99c0ca5988
Gabrielly Baião <103866012+GabriellyBaiao@users.noreply.github.com>
2024-07-03 11:16:40 -0300

    [PTI] feat: Creating a "Pure" Pipe

--- 06_pipes/src/app/app.module.ts
+++ 06_pipes/src/app/app.module.ts
@@ -5,29 +5,33 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
 import { CamelCasePipe } from './camel-case.pipe';
-import { SettingsService} from './settings.service'
+import { SettingsService} from './settings.service';
+import { FiltroArrayPipe } from './filtro-array.pipe'
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     ExemplosPipesComponent,
-    CamelCasePipe
+    CamelCasePipe,
+    FiltroArrayPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
     // {
     //   provide: LOCALE_ID,
     //   useValue: 'pt-BR'
     // }
-    SettingsService,
-    {
-      provide: LOCALE_ID,
-      deps: [SettingsService],
-      useFactory: (settingsService: SettingsService) => settingsService.getLocale()
-    }
+    // SettingsService,
+    // {
+    //   provide: LOCALE_ID,
+    //   deps: [SettingsService],
+    //   useFactory: (settingsService: SettingsService) => settingsService.getLocale()
+    // }
   ],
   bootstrap: [AppComponent]
 })
