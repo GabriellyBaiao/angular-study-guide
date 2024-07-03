2c2d49c8ffabc4ae24c40fefde099a59afcdb819
Gabrielly Baião <103866012+GabriellyBaiao@users.noreply.github.com>
2024-07-03 09:38:38 -0300

    [PTI] feat: Creating a pipe

---
+++ 06_pipes/src/app/camel-case.pipe.ts
@@ -0,0 +1,26 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'camelCase'
+})
+export class CamelCasePipe implements PipeTransform {
+
+  transform(value: unknown, ...args: unknown[]): unknown {
+    if (typeof value !== 'string') {
+      return value;
+    }
+
+    let values = value.split(' ');
+    let result = '';
+
+    for (let v of values) {
+      result += this.capitalize(v) + ' ';
+    }
+
+    return result.trim();
+  }
+
+  capitalize(value: string): string {
+    return value.substr(0, 1).toUpperCase() + value.substr(1).toLowerCase();
+  }
+}
