12fa9d07dc9b322c200fb90fd4d24f413f9fdd8f
Gabrielly Baião <103866012+GabriellyBaiao@users.noreply.github.com>
2024-07-04 11:25:00 -0300

    [PTI] feat: Routes - Creating a routing module

---
+++ 07_rotas/src/app/app.routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+//import { CursosComponent } from './cursos/cursos.component';
+import { LoginComponent } from './login/login.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
+//import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
+//import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', component: HomeComponent },
+  { path: 'cursos', component: CursosComponent },
+  { path: 'curso/:id', component: CursoDetalheComponent },
+  { path: 'naoEncontrado', component: CursoNaoEncontradoComponent}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(appRoutes, {useHash: true})],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
