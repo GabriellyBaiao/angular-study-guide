1e0f1a241eb2a2897a90b65251023373c22d5124
Gabrielly Baião <103866012+GabriellyBaiao@users.noreply.github.com>
2024-07-07 15:18:06 -0300

    [PTI] feat: Child Routes - Developing the Screens

--- 07_rotas/src/app/alunos/aluno-detalhe/aluno-detalhe.component.ts
+++ 07_rotas/src/app/alunos/aluno-detalhe/aluno-detalhe.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { AlunosService } from './../alunos.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscribable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-aluno-detalhe',
   templateUrl: './aluno-detalhe.component.html',
   styleUrl: './aluno-detalhe.component.css'
 })
-export class AlunoDetalheComponent {
+export class AlunoDetalheComponent implements OnInit, OnDestroy {
+
+  aluno: any;
+  inscricao!: Subscription;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private alunosService: AlunosService
+  ){}
+
+  ngOnInit() {
+    this.inscricao = this.route.params.subscribe(
+      (params: any) => {
+        let id = params['id'];
+
+        this.aluno = this.alunosService.getAluno(id);
+      }
+    );
+  }
+
+  editarContato(){
+    this.router.navigate(['/alunos', this.aluno.id, 'editar']);
+  }
+
+  ngOnDestroy(){
+    this.inscricao.unsubscribe();
+  }
 
 }
