e7e83000157f6f272319cd0233a7b4b052207b3e
Gabrielly Baião <103866012+GabriellyBaiao@users.noreply.github.com>
2024-07-15 16:03:19 -0300

    [PTI] feat: Forms (Tip): Checking Form Data in the Template with JSON

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { DataFormComponent } from './data-form/data-form.component';
 import { FormsModule } from '@angular/forms';
 import { TemplateFormModule } from './template-form/template-form.module';
+import { FormDebugComponent } from './form-debug/form-debug.component';
 
 @NgModule({
   declarations: [
