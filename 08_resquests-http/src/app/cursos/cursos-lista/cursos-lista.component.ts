ae05d01372931bac0e86f7d4997bb5bd2c44f46d
Gabrielly Baião <103866012+GabriellyBaiao@users.noreply.github.com>
2024-07-19 20:46:13 -0300

    [PTI] feat: Capturing Errors (+ Error with async)

--- 08_resquests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ 08_resquests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CursosService } from '../cursos.service';
 import { Curso } from './curso';
-import { Observable } from 'rxjs';
+import { Observable,  Subject, EMPTY } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-cursos-lista',
@@ -14,6 +15,7 @@ export class CursosListaComponent implements OnInit {
   // cursos: Curso[] = [];
 
   cursos$!: Observable<Curso[]>;
+  error$ = new Subject<boolean>();
 
   constructor(private service: CursosService){}
 
@@ -22,6 +24,20 @@ export class CursosListaComponent implements OnInit {
     //   .subscribe(
     //     dados => this.cursos = dados, // Atribuindo os dados à variável cursos
     //   );
-    this.cursos$ = this.service.list();
+    this.onRefresh();
+  }
+
+  onRefresh(){
+    this.cursos$ = this.service.list()
+    .pipe(
+      //map(),
+      //tap(),
+      //switchMap(),
+      catchError(error =>{
+        console.error(error);
+        this.error$.next(true);
+        return EMPTY;
+      })
+    )
   }
 }
