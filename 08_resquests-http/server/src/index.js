e5cda798e5d93f304fff10fd8077a20966e69fe4
Gabrielly Baião <103866012+GabriellyBaiao@users.noreply.github.com>
2024-08-04 14:10:44 -0300

    [PTI] feat: Http File Upload Request with FormData

--- 08_resquests-http/server/src/index.js
+++ 08_resquests-http/server/src/index.js
@@ -7,19 +7,18 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const corsOptions = {
-  origin: '*',
-  optionSucessStatus: 200,
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//   origin: '*',
+//   optionsSuccessStatus: 200
+// };
+// app.use(cors(corsOptions));
 
-const multipartMiddleware = multipart({ upLoadDir: './uploads'});
+const multipartMiddleware = multipart({ uploadDir: './uploads' });
 app.post('/upload', multipartMiddleware, (req, res) => {
   const files = req.files;
   console.log(files);
   res.json({ message: files });
 });
-
 app.use((err, req, res, next) => res.json({error: err.message}));
 
 app.listen(8000, () => {
--- 08_resquests-http/src/app/upload-file/upload-file/upload-file.component.ts
+++ 08_resquests-http/src/app/upload-file/upload-file/upload-file.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UploadFileService } from '../upload-file.service';
 
 @Component({
   selector: 'app-upload-file',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadFileComponent implements OnInit {
 
-  constructor() {}
+  files!: Set<File>;
+
+  constructor(private service: UploadFileService){}
 
   ngOnInit() {
   }
@@ -19,13 +22,22 @@ export class UploadFileComponent implements OnInit {
     // document.getElementById('customFileLabel').innerHTML = selectedFiles[0].name;
 
     const fileNames = [];
+    this.files = new Set();
     for (let i = 0; i < selectedFiles.length; i++) {
       fileNames.push(selectedFiles[i].name);
+      this.files.add(selectedFiles[i]);
     }
 
     const customFileLabel = document.getElementById('customFileLabel');
     if (customFileLabel) {
       customFileLabel.innerHTML = fileNames.join(', ');
+    }
+  }
+
+  onUpload() {
+    if(this.files && this.files.size > 0){
+      this.service.upload(this.files, 'http://localhost:8000/upload')
+        .subscribe(response => console.log('Upload Concluído'))
     }
   }
 }
